69a4cda8b8cb4915c2bf610a67b2d11bc88196fe
Wentao <77952995+Wentao-Gao@users.noreply.github.com>
2024-01-08 22:59:54 +1030

    Delete scripts/Weathercase.sh

--- scripts/Weathercase.sh
+++
@@ -1,6 +0,0 @@
-for seed in $(seq 0 4); do
-  # multivar
-  python -u train.py weather_case forecast_multivar --epochs 10 --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv --repr-dims 320 --max-threads 8 --seed ${seed} --eval
-  # univar
-  python -u train.py weather_case forecast_univar --epochs 10 --alpha 0.0005 --kernels 1 2 4 8 16 32 64 128 --max-train-length 201 --batch-size 128 --archive forecast_csv_univar --repr-dims 320 --max-threads 8 --seed ${seed} --eval
-done
